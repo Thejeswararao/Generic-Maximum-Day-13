fe3c75f9f234f74d957a3d98bbedb53bfe44b6ec
Thejeswararao <thejeswararaom@gmail.com>
2021-12-21 20:06:20 +0530

    Modofied:We Write the test case for testing the result which is Maximum number at first place...

--- Maximum.java
+++ Maximum.java
@@ -1,29 +1,18 @@
 package bridgelabz.com;
 
-public class Maximum<E extends Comparable> {
+public class Maximum {
 
-	E value1;
-	E value2;
-	E value3;
-
-	public Maximum(E value1, E value2, E value3) {
-		this.value1 = value1;
-		this.value2 = value2;
-		this.value3 = value3;
-	}
-
-	public E findMaximum() {
-		return Maximum.findMaximum(value1, value2, value3);
-	}
-
-	public static< E extends Comparable> E findMaximum(E value1, E value2, E value3) {
-
-		E max = value1;
-		if (value2.compareTo(value1)>0 && value2.compareTo(value3)>0) {
-			max = value2;
-		} if (value3.compareTo(max) > 0) {
-			max = value3;
+	public static Integer findMaximum(Integer a, Integer b, Integer c) {
+		Integer max = a;
+		if (b.compareTo(a)>0 && b.compareTo(c)>0) {
+			max = b;
+		} if (c.compareTo(max) > 0) {
+			max = c;
 		}
-		return (max);
+		return max;
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Welcome Generics Maximum program");
 	}
 }
