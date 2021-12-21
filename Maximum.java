58f96edcd3faba8ee99c75f1e2781f167b6b1b6a
Thejeswararao <thejeswararaom@gmail.com>
2021-12-21 20:50:19 +0530

    Added:by using Compare To we find the Maximum of three Strings...

--- Maximum.java
+++ Maximum.java
@@ -1,17 +1,18 @@
 package bridgelabz.com;
 
 public class Maximum {
-	public static Float findMaximum(Float a, Float b, Float c) {
-		Float max = a;
+	public static String findMaximum(String a, String b, String c) {
+		String max = a;
 		if (b.compareTo(a)>0 && b.compareTo(c)>0) {
 			max = b;
 		} if (c.compareTo(max) > 0) {
 			max = c;
 		}
-		return max;
+		return (max);
 	}
 
 	public static void main(String[] args) {
 		System.out.println("Welcome Generics Maximum program");
 	}
+
 }
