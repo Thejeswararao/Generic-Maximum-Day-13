1ce3f13c74bacf0acbd486606514653c8f8ddd77
Thejeswararao <thejeswararaom@gmail.com>
2021-12-21 19:56:01 +0530

    Modofied:Now we declare generic as a class...

--- Maximum.java
+++ Maximum.java
@@ -1,17 +1,29 @@
 package bridgelabz.com;
 
-public class Maximum {
-	public static<E extends Comparable>E findMaximum(E a, E b, E c) {
-		E max = a;
-		if (b.compareTo(a)>0 && b.compareTo(c)>0) {
-			max = b;
-		} if (c.compareTo(max) > 0) {
-			max = c;
-		}
-		return (max);
+public class Maximum<E extends Comparable> {
+
+	E value1;
+	E value2;
+	E value3;
+
+	public Maximum(E value1, E value2, E value3) {
+		this.value1 = value1;
+		this.value2 = value2;
+		this.value3 = value3;
 	}
 
-	public static void main(String[] args) {
-		System.out.println("Welcome Generics Maximum program");
+	public E findMaximum() {
+		return Maximum.findMaximum(value1, value2, value3);
+	}
+
+	public static< E extends Comparable> E findMaximum(E value1, E value2, E value3) {
+
+		E max = value1;
+		if (value2.compareTo(value1)>0 && value2.compareTo(value3)>0) {
+			max = value2;
+		} if (value3.compareTo(max) > 0) {
+			max = value3;
+		}
+		return (max);
 	}
 }
