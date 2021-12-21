11cf09edd12ed733de8c9ccbaf613a4232d7f956
Thejeswararao <thejeswararaom@gmail.com>
2021-12-21 19:49:48 +0530

    Modofied:Instead of giving three datatype methods we gave a single...

---
+++ Maximum.java
@@ -0,0 +1,17 @@
+package bridgelabz.com;
+
+public class Maximum {
+	public static<E extends Comparable>E findMaximum(E a, E b, E c) {
+		E max = a;
+		if (b.compareTo(a)>0 && b.compareTo(c)>0) {
+			max = b;
+		} if (c.compareTo(max) > 0) {
+			max = c;
+		}
+		return (max);
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Welcome Generics Maximum program");
+	}
+}
