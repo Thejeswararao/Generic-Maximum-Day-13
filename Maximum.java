d1c5d5e445e70873aeac8ff14c89d5fd78816447
Thejeswararao <thejeswararaom@gmail.com>
2021-12-21 20:46:34 +0530

    Added:by using Compare To we find the Maximum of three floats...

--- Maximum.java
+++ Maximum.java
@@ -1,19 +1,17 @@
 package bridgelabz.com;
 
 public class Maximum {
-	public static Integer findMaximum(Integer a, Integer b, Integer c) {
-		Integer max = a;
+	public static Float findMaximum(Float a, Float b, Float c) {
+		Float max = a;
 		if (b.compareTo(a)>0 && b.compareTo(c)>0) {
 			max = b;
 		} if (c.compareTo(max) > 0) {
 			max = c;
 		}
-		System.out.println(max);
 		return max;
 	}
 
 	public static void main(String[] args) {
 		System.out.println("Welcome Generics Maximum program");
-		Maximum.findMaximum(9,4,6);
 	}
 }
